ea9af6d0e35fb12a3efd4034df2aaf4c77ca6b02
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-06-28 16:15:56 -0600

    added root path.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: "pages#contact"
   resources :blogs
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
