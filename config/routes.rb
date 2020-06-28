ea0d47aa751ec07a175a6842acbc44423397d8db
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-06-28 16:21:23 -0600

    added namespace.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  
   get 'about', to: 'pages#about'
   get 'contact', to: "pages#contact"
   resources :blogs
-
+  
+  namespace :admin do
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
